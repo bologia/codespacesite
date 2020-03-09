dd3bae0c08311b6eda88299ac20566cbd5214feb
bologia <gusberti.simplon@gmail.com>
2020-03-09 13:39:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php include('base.php');?>
+
+<div>
+    <br><br><br><br><br><br><br><br><p style="margin-left: 50%;">?</p><br><br><br><br><br><br><br><br>
+</div>
+<hr class="hrlol" style="width: 90%;">
+<br>
+
+<!--Présentation de qqch-->
+
+<div>
+    <h2 align="center">Présentation de qqch</h2><br>
+    <p style="margin: 1% 25%;" align="center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.<br><br>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.
+    </p>
+</div>
+<br><br>
+<div class="d-flex flex-wrap justify-content-center">
+    <img src="images/imagmultimediacode.jpg" alt="" width="700px" height="500px"/>
+</div>
+
+<!--Fin de Présentation de qqch-->
+
+<hr class="hrlol" style="width: 90%;">
+<br>
+
+<!--Truc fontawesome-->
+
+<div class="row mx-auto container"><!--d-flex flex-wrap justify-content-around align-items-center-->
+    <div class="col-md-6">
+        <div class="d-flex">
+            <i class="fas fa-mobile-alt fa-4x" style="color: #2D00D1; margin-left: 15px; margin-right: 57px;"></i>
+            <p>Entreprise de l’économie sociale et solidaire (bonjour teddy).
+            </p>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="d-flex">
+            <i class="fas fa-desktop fa-4x mr-5" style="color: #2D00D1;"></i>
+            <p>Entreprise de l’économie sociale et solidaire (agrément ESUS), Simplon.co est un réseau de « fabriques » 
+            </p>
+        </div>
+    </div>
+</div>
+
+<br><br><br><br>
+
+<div class="row mx-auto container">
+    <div class="col-md-6">
+        <div class="d-flex">
+            <i class="fas fa-camera fa-4x mr-5" style="color: #2D00D1;"></i>
+            <p>Entreprise de l’économie sociale et solidaire (bonjour teddy). lorem on continue de faire semblant je sais pas quoi ecrire lol mdr mr codespace fait nimp.
+            </p>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="d-flex">
+            <i class="fas fa-mouse fa-4x" style="color: #2D00D1; margin-left: 12px; margin-right: 60px;"></i>
+            <p>Entreprise de l’économie sociale et solidaire (agrément ESUS), Simplon.co !
+            </p>
+        </div>
+    </div>
+</div>
+
+<!--Fin de Truc fontawesome-->
+
+<hr class="hrlol" style="width: 90%;">
+
+<!--slide-->
+
+<div class="container" align="center">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block w-60" src="images/examplesiteweb.jpg" width="800px" height="500px" alt="First slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-60" src="images/netflixsiteweb.jpg" width="800px" height="500px" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-60" src="images/greensiteweb.png" width="800px" height="500px" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+<!--Fin de slide-->
+
+<hr class="hrlol" style="width: 90%;">
+<br><br><br>
+
+<?php include('footer.php');?>
