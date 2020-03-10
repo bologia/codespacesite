b17bd9c8e6cc70dca2c251e9a33d168096fb71a6
bologia <gusberti.simplon@gmail.com>
2020-03-10 13:38:52 +0100

    avancement home

--- includes/home.php
+++ includes/home.php
@@ -1,4 +1,16 @@
-<div class="s1 p-5">
-    <h1>Test de l'écriture bold</h1>
-    <h2>Test de l'écriture light</h2>
+<div class="s1 d-flex">
+    
+    <div class="card-deck mx-auto align-self-center">
+        <div class="card rounded-0">
+            <div class="card-body text-center">
+                <h1>Bienvenue sur CodeSpace</h1><br><br>
+                <h5>Développeurs web, e-commerce et application.</h5><br>
+                <h5>Nous répondons à vos besoins pour toutes vos demandes de création de site web ainsi que de dépannage informatique.</h5><br>
+                <h5>Contactez nous pour plus d'informations.</h5>
+            </div>
+            <button type="button" class="btn btn-info btn-circle btn-xl"><p>Envoie</p></button>
+        </div>
+    </div>
+
 </div>
+
