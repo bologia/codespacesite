f2d2d315f9cce7126ec3e70472e3163de6fbad95
bologia <gusberti.simplon@gmail.com>
2020-03-10 16:58:21 +0100

    fignolage home 10/03

--- includes/home.php
+++ includes/home.php
@@ -1,16 +1,12 @@
-<div class="s1 d-flex">
-    
-    <div class="card-deck mx-auto align-self-center">
-        <div class="card rounded-0">
-            <div class="card-body text-center">
-                <h1>Bienvenue sur CodeSpace</h1><br><br>
-                <h5>Développeurs web, e-commerce et application.</h5><br>
-                <h5>Nous répondons à vos besoins pour toutes vos demandes de création de site web ainsi que de dépannage informatique.</h5><br>
-                <h5>Contactez nous pour plus d'informations.</h5>
-            </div>
-            <button type="button" class="btn btn-info btn-circle btn-xl"><p>Envoie</p></button>
+<div class="s1 d-flex justify-content-center align-items-center">
+    <div class="card-home card rounded-0">
+        <div class="card-body text-center">
+            <h1>Bienvenue sur</h1>
+            <img src="./img/codespacelogoname.png" alt="Logo CodeSpace" width="80%"><br><br><br>
+            <h5>Développeurs web, e-commerce et application.</h5><br>
+            <h5>Nous répondons à vos besoins pour toutes vos demandes de création de site web ainsi que de dépannage informatique.</h5><br>
+            <h5>Contactez nous pour plus d'informations.</h5>
         </div>
+        <button type="button" class="btn btn-info btn-home"><i class="fas fa-envelope"></i></button>
     </div>
-
 </div>
-
