8831fd8a43fe0303d860b918dfd986dabb39cf4d
Teddy Couaillier <t.couaillier@simplon-charleville.fr>
2020-03-10 13:42:48 +0100

    Nav created + logo/favicon

---
+++ includes/nav.php
@@ -0,0 +1,19 @@
+<div class="main-nav d-flex flex-column justify-content-between">
+    <div>
+        <img src="./img/cs.png" alt="Logo CodeSpace" width="65%">
+        <img src="./img/codespacelogoname.png" alt="Logo CodeSpace" width="53%">
+    </div>
+    <div>
+        <ul class="list-group list-group-flush font-weight-bold nav-list">
+            <a href="#"><li class="list-group-item">Accueil</li></a>
+            <a href="#"><li class="list-group-item">Qui sommes-nous ?</li></a>
+            <a href="#"><li class="list-group-item">Services</li></a>
+            <a href="#"><li class="list-group-item">Liste des projets</li></a>
+            <a href="#"><li class="list-group-item">Contact</li></a>
+        </ul>
+    </div>
+    <div>
+        <i class="fas fa-phone"></i> &nbsp; <span class="number">06.30.30.30.30</span><br>
+        <i class="fas fa-at"></i> &nbsp; contact@codespace.com
+    </div>
+</div>
