140098a1c27cbd2d6fa2ac457532bf4cae49edda
Teddy Couaillier <t.couaillier@simplon-charleville.fr>
2020-03-11 17:30:49 +0100

    Update "contact" section

---
+++ includes/contact.php
@@ -0,0 +1,67 @@
+<div class="contact d-flex justify-content-between p-2" style="background: #efefef;">
+    <form class="bg-white p-5 rounded">
+        <div class="form-group">
+            <label for="adressemail">Adresse mail</label>
+            <input type="email" class="form-control" id="adressemail" placeholder="adresse@mail.com">
+        </div>
+        <div class="form-group">
+            <label for="subject">Sujet</label>
+            <input type="text" class="form-control" id="subject" placeholder="Demande d'informations">    
+        </div>
+        <div class="form-group">
+            <label for="message">Votre message</label>
+            <textarea class="form-control" id="message" placeholder="votre message" rows="5"></textarea>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn">Envoyer message</button>
+        </div>
+    </form>
+    <div class="contact-other rounded bg-white">
+        <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center border-0">
+                <p class="my-0">
+                    <i class="fab fa-facebook-f mr-2"></i>
+                    Facebook
+                </p>
+                @CodeSpace
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center border-0">
+                <p class="my-0">
+                    <i class="fab fa-linkedin mr-2"></i>
+                    LinkedIn
+                </p>
+                @CodeSpace
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center border-0">
+                <p class="my-0">
+                    <i class="fab fa-twitter mr-2"></i>
+                    Twitter
+                </p>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center border-0">
+                <p class="my-0">
+                    <i class="fas fa-phone mr-2"></i>
+                    <span class="number">06.50.50.50.50</span>
+                </p>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center border-0">
+                <p class="my-0">
+                    <i class="fas fa-map-marker-alt mr-2"></i>
+                    Adresse
+                </p>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center border-0">
+                <p class="my-0">
+                    <i class="fas fa-globe mr-2"></i>
+                    Site internet
+                </p>
+            </li>
+        </ul>
+        <hr class="mx-3">
+        <p class="px-3">
+            <b class="h4">EQUIPE</b><br><br>
+            GUSBERTI Gianni - <i>Bac+<span class="number">3</span> Développement web et web mobile</i><br><br>
+            COUAILLIER Teddy - <i>Bac+<span class="number">3</span> Développement web et web mobile</i>
+        </p>
+    </div>
+</div>
